5041b488c9d971b63189cdf9898f96140a5c5b49
阿杰 <master@x-ac.cn>
2026-09-24 09:44:08 +0800

    解决偶发闪屏问题 完善路由中设置标题多语言

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,35 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
+import { t } from '@/i18n/index.js';
+import Layout from '@/layout/index.vue';
 
 const routes = [{
   path:      '/',
   name:      'index',
-  component: () => import('@/layout/index.vue'),
+  component: Layout,
   redirect:  {name: 'downloading'},
   children:  [
     {
       path:      '/downloading',
       name:      'downloading',
+      meta:      {title: 'menu.downloading'},
       component: () => import('@/views/index.vue'),
     },
     {
       path:      '/waiting',
       name:      'waiting',
+      meta:      {title: 'menu.waiting'},
       component: () => import('@/views/waiting.vue'),
     },
     {
       path:      '/stopped',
       name:      'stopped',
+      meta:      {title: 'menu.stopped'},
       component: () => import('@/views/stopped.vue'),
     },
     {
       path:      '/settings',
       name:      'settings',
+      meta:      {title: 'menu.settings'},
       component: () => import('@/views/settings.vue'),
     },
   ],
@@ -39,7 +45,7 @@ router.beforeEach(function (to, from) {
   console.log('beforeEach', to, from);
 });
 router.afterEach(function (to, from) {
-  document.title = to?.meta?.title || 'Aria2 Web UI';
+  document.title = t(to?.meta?.title || 'Aria2 Web UI');
   console.log('afterEach', to, from);
 });
 
